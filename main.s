10d188f1e1ebfafb8a7afb3c0269abc773f4a496
MakerFace <chenqi@imudges.com>
2022-11-02 10:10:38 +0800

    寄存器使用

--- main.s
+++ main.s
@@ -1,19 +1,24 @@
 
 
 segment   .text
-global _start ;declare for linker(ld)
+global _start   ;declare for linker(ld)
 
 
-_start:; main func
+_start:         ; main func
   mov   edx,len
   mov   ecx,msg
-  mov   eax,1  ; file descriptor (stdout)
-  mov   eax,4  ; system call number(sys_write)
-  int   0x80   ; call kernel
-  mov   eax,1  ; system call number(sys_exit)
-  int   0x80   ; call kernel
+  mov   eax,1   ; file descriptor (stdout)
+  mov   eax,4   ; system call number(sys_write)
+  int   0x80    ; call kernel
+  mov   edx,9   ; message length
+  mov   ecx,s2  ; message to write
+  mov   ebx,1   ; file descriptor (stdout)
+  mov   eax,4   ; system call number(sys_write)
+  int   0x80    ; call
+  mov   eax,1   ; system call number(sys_exit)
+  int   0x80    ; call kernel
 
 section   .data
 msg db 'Hello World!',0xa  ;string to be printed
-len equ $ - msg            ;length of string
-
+len equ $ - msg            ;* length of string
+s2 times 9 db '*' ; ?产生9个*
