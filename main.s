e51bf044931479d533731eb9a795ea65f37304e1
MakerFace <chenqi@imudges.com>
2022-11-03 21:22:15 +0800

    不用stdout也能输出？

--- main.s
+++ main.s
@@ -7,7 +7,7 @@ global _start   ;declare for linker(ld)
 _start:         ; main func
   mov   edx,len
   mov   ecx,msg
-  mov   eax,1   ; file descriptor (stdout)
+  mov   ebx,1   ; file descriptor (stdout)
   mov   eax,4   ; system call number(sys_write)
   int   0x80    ; call kernel
   mov   edx,9   ; message length
