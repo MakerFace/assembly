03583598cc2301faa794897d9afe75dd5de7d91b
MakerFace <chenqi@imudges.com>
2022-11-04 23:29:52 +0800

    logical instruction

--- logical_instructions.asm
+++ logical_instructions.asm
@@ -1,21 +1,32 @@
-	segment .data
-	num dw 0x00111010
+	extern printf
+	
+	section .bss
+	addr resb 4
+	
+	section .data
+	num db 0x63
+    fmt db "0x%x", 10, 0
 	
 	section .text
 	global _start
 	
-_start:
 	
-	mov cl, num
-	and cl, 0fh
-
-    mov edx, 1
-    mov ecx, 'a'
-
-    mov ebx, 1
-    mov eax, 4
-    int 80h
-
-    mov ebx, 0
-    mov eax, 1
-    int 80h
+_start:
+	mov eax, num
+    mov [addr], dword eax
+    
+    push dword [addr]
+    push dword fmt
+    call printf
+    add esp, 8
+	
+	mov edx, 1
+	mov ecx, num
+	
+	mov ebx, 1
+	mov eax, 4
+	int 80h
+	
+	mov ebx, 0
+	mov eax, 1
+	int 80h
